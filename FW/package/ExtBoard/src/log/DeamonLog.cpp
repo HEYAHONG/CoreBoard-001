a8fa3f154d4f3860cabc61b45836e12585850322
HEYAHONG <2229388563@qq.com>
2024-05-06 17:38:20 +0800

    [ExtBoardD] 添加对syslog的初始化与反初始化

--- FW/package/ExtBoard/src/log/DeamonLog.cpp
+++ FW/package/ExtBoard/src/log/DeamonLog.cpp
@@ -1,11 +1,16 @@
 #include "DeamonLog.h"
+#include "syslog.h"
 
 void DeamonLog_Init()
 {
+    //初始化 syslog
+    openlog("ExtBoardD", LOG_NDELAY|LOG_NOWAIT|LOG_PID, LOG_DAEMON);
     LOGINFO("DeamonLog Init!");
 }
 
 void DeamonLog_Deinit()
 {
     LOGINFO("DeamonLog Deinit!");
+    //关闭syslog
+    closelog();
 }
